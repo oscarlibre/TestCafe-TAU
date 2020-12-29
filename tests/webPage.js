ddec82c401f686ec5db7eb41ac9959f9f6ebeae0
oscarlibre <oscarlibre@gmail.com>
2020-12-29 18:07:30 -0500

    Add files via upload
    Test that specifies the start webpage

---
+++ tests/webPage.js
@@ -0,0 +1,12 @@
+
+fixture("First Fixture")
+    .page("https://www.google.com");
+
+test.page("https://devexpress.github.io/testcafe/example/")
+("First Test", async t=>{
+    await t 
+        .typeText("#developer-name", "TAU")
+        .click("#macos")
+        .click("#submit-button");
+
+});
